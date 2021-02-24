61bdc019297967c0a2cdfae04ed3b7a325f2a3ec
mulan-94 <andatilexy@gmail.com>
2021-02-24 04:31:40 +0200

    Remove unnecesary files Moved mouseup function to on_load.js Updated js9prefs files

--- static/JS9/js9prefs.js
+++ static/JS9/js9prefs.js
@@ -1,106 +1,35 @@
 var JS9Prefs = {
-  "globalOpts": {
-        helperType: "nodejs",
-        helperPort: 80, 
-        helperCGI: "./cgi-bin/js9/js9Helper.cgi",
-        // fits2png: false,
-        debug: 0,
-        loadProxy: true,
-        workDir: "../tmp",
-        workDirQuota: 300,
-        dataPath: "$HOME/Desktop:$HOME/data",
-        analysisPlugins: "./analysis-plugins",
-        analysisWrappers: "./analysis-wrappers",
-        fits2fits: "always",
-        image: {xdim: 4096, ydim: 4096, bin: 1},
-        table: 30,
-        mousetouchZoom: true,
-        mouseActions: ["display value/position", "pan the image", "change contrast/bias"],
-        touchActions: ["display value/position", "pan the image", "change contrast/bias"],
-        resetEmptyShapeId: true
-        },
+	"globalOpts": {
+		"helperType": "nodejs",
+		"helperPort": 2718,
+		"helperCGI": "./cgi-bin/js9/js9Helper.cgi",
+		"fits2png": false,
+		"debug": 10,
+		"loadProxy": true,
+		"workDir": "./tmp",
+		"workDirQuota": 100,
+		"fits2fits": false,
+		"image": { "xdim": 4096, "ydim": 4096, "bin": 4},
+		"table": { "xdim": 4096, "ydim": 4096, "bin": 4 },
+		"dataPath": "./../images",
+		"analysisPlugins": "./analysis-plugins",
+		"analysisWrappers": "./analysis-wrappers",
+		"mousetouchZoom": true,
+		"mouseActions": ["display value/position", "pan the image", "change contrast/bias"],
+		"touchActions": ["display value/position", "pan the image", "change contrast/bias"],
+		"resetEmptyShapeId": true,
+		"requireFits2Fits": false
+	},
 
-  "imageOpts":  {
-        colormap: "inferno",
-        scale:    "linear",
-        crosshair: true,
-        zoom: "tofit"
-    },
-    
-    "regionOpts": {
-        panzoom: true,
-        onmouseup: function loadPlot(im, xreg){
-            function removeAllChildNodes(parent){
-                while (parent.firstChild){
-                    parent.removeChild(parent.firstChild);
-                }
-                return parent;
-            }
+	"imageOpts": {
+		"colormap": "inferno",
+		"scale": "linear",
+		"crosshair": true,
+		"zoom": "tofit"
+	},
 
-            function loadPlot(regionId, containerId, colour){
-
-                let plotPath = "/static/plots/reg";
-                let plotContainer = document.getElementById(containerId);
-                let plotTitle = document.createElement("div");
-
-                JS9.ChangeRegions(regionId, {color: colour});
-
-                plotTitle.innerHTML = `Region ${regionId} <span style="display:inline-block; width:20px; height:20px; border-radius:50%; background-color:${colour}; vertical-align: middle;"></span>`;
-                plotTitle.style = "font-family: monospace; font-size: 13px;";
-                
-                let iframe = document.createElement("iframe");
-                Object.assign(iframe, {
-                    // sandbox: "allow-same-origin allow-scripts",
-                    width: "97vw",
-                    height: "190%", scrolling: "no",
-                    seamless: "seamless", frameborder: "0"
-                });
-                iframe.src = `http://${location.host}/${plotPath}${regionId}.html`;
-
-                plotContainer = removeAllChildNodes(plotContainer);
-                plotContainer.dataset.id = regionId;
-                plotContainer.title = `Plot for region ${regionId}`;
-                plotContainer.appendChild(plotTitle);
-                plotContainer.appendChild(iframe);
-            }
-
-            function switchPosition(clickedReg){
-                //get the incumbent's data id
-
-                let currentPlot = document.getElementById("current-plot");
-                let previousPlot = document.getElementById("previous-plot");
-
-                // get region IDs for the plots stored in the id variable
-                let currentRegId = currentPlot.dataset.hasOwnProperty("id") ? currentPlot.dataset.id : -1;
-                let previousRegId = previousPlot.dataset.hasOwnProperty("id") ? previousPlot.dataset.id : -1;
-
-                if (previousRegId != -1) {
-                    JS9.ChangeRegions(previousRegId, { color: "black" });
-                }
-
-                currentPlot = removeAllChildNodes(currentPlot);
-                previousPlot = removeAllChildNodes(previousPlot);
-                
-                //load a plot to previous plot's region if there's a current plot
-                if (currentRegId != -1){
-                    loadPlot(currentRegId, "previous-plot", "white");
-                }
-                loadPlot(clickedReg, "current-plot", "#A907F8");
-
-            }
-            
-            // adding 1 because JS9 assigns IDs from 1 and plots are tagging from 0
-            let xregTagNum = Number(xreg.tags[0]) + 1;
-
-            if (Number.isInteger(xregTagNum)){
-                console.log(`Region ID: ${xreg.id} ${xregTagNum == xreg.id ? "does" : "doesn't"} match tag number: ${xregTagNum}`);
-                JS9.ChangeRegions(tags=xregTagNum, { color: "#A907F8", strokeWidth: 8});
-                switchPosition(xregTagNum);
-            }
-            else{
-                alert("No plots available for the selected region");
-            }
-
-            }
-    }
+	"regionOpts": {
+		"panzoom": true,
+		"onmouseup": lodLosPlots
+	}
 }
---
+++ static/javascript/on_load.js
@@ -0,0 +1,97 @@
+/*
+Load the cygnus image and region file after JS9 finishes loading
+*/
+
+
+function removeAllChildNodes(parent){
+        while (parent.firstChild){
+            parent.removeChild(parent.firstChild);
+        }
+        return parent;
+    }
+    
+
+function switchPosition(clickedReg){
+        //get the incumbent's data id
+
+        let currentPlot = document.getElementById("current-plot");
+        let previousPlot = document.getElementById("previous-plot");
+
+        // get region IDs for the plots stored in the id variable
+        let currentRegId = currentPlot.dataset.hasOwnProperty("id") ? currentPlot.dataset.id : -1;
+        let previousRegId = previousPlot.dataset.hasOwnProperty("id") ? previousPlot.dataset.id : -1;
+
+        if (previousRegId != -1) {
+            JS9.ChangeRegions(previousRegId, { color: "black" });
+        }
+
+        currentPlot = removeAllChildNodes(currentPlot);
+        previousPlot = removeAllChildNodes(previousPlot);
+        
+        //load a plot to previous plot's region if there's a current plot
+        if (currentRegId != -1){
+            loadPlot(currentRegId, "previous-plot", "white");
+        }
+        loadPlot(clickedReg, "current-plot", "#A907F8");
+
+    }
+
+
+function loadPlot(regionId, containerId, colour){
+
+        let plotPath = "cygnus/plots/reg";
+        let plotContainer = document.getElementById(containerId);
+        let plotTitle = document.createElement("div");
+
+        JS9.ChangeRegions(regionId, {color: colour});
+
+        plotTitle.innerHTML = `Region ${regionId} <span style="display:inline-block; width:20px; height:20px; border-radius:50%; background-color:${colour}; vertical-align: middle;"></span>`;
+        plotTitle.style = "font-family: monospace; font-size: 13px;";
+        
+        let iframe = document.createElement("iframe");
+        Object.assign(iframe, {
+            // sandbox: "allow-same-origin allow-scripts",
+            width: "97vw",
+            height: "190%", scrolling: "no",
+            seamless: "seamless", frameborder: "0"
+        });
+        iframe.src = `http://${location.host}/${plotPath}${regionId}.html`;
+
+        plotContainer = removeAllChildNodes(plotContainer);
+        plotContainer.dataset.id = regionId;
+        plotContainer.title = `Plot for region ${regionId}`;
+        plotContainer.appendChild(plotTitle);
+        plotContainer.appendChild(iframe);
+    }
+
+
+function lodLosPlots(im, xreg){
+    let xregTagNum = Number(xreg.tags[0]) + 1;
+
+    if (Number.isInteger(xregTagNum)){
+        console.log(`Region ID: ${xreg.id} ${xregTagNum == xreg.id ? "does" : "doesn't"} match tag number: ${xregTagNum}`);
+        JS9.ChangeRegions(tags=xregTagNum, { color: "#A907F8", strokeWidth: 8});
+        switchPosition(xregTagNum);
+    }
+    else{
+        alert("No plots available for the selected region");
+    }
+
+}
+
+
+function loadLosRegs(){
+        JS9.LoadRegions("images/cyg_los.reg");
+}
+
+
+function initialiseCygnus(){
+    JS9.CloseImage({clear: true});
+    JS9.Preload("images/CYG-0.75-SLO-I.FITS",
+        {
+            "zoom": "toFit", "colormap": "inferno",
+            "scale": "linear", "scalemin": -0.009,
+            "scalemax": 0.5, "onload": loadLosRegs,
+            "fits2fits": true
+        });
+}
