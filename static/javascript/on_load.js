b02a445bbd0d9e8fb3e87ed817cc62ff3fd4a3fb
mulan-94 <andatilexy@gmail.com>
2021-02-27 16:06:20 +0200

    Add JS for the new extra plot displayed Add a fade animation for the plots Add a new tab icon

--- static/javascript/on_load.js
+++ static/javascript/on_load.js
@@ -4,65 +4,72 @@ Load the cygnus image and region file after JS9 finishes loading
 
 
 function removeAllChildNodes(parent){
-        while (parent.firstChild){
-            parent.removeChild(parent.firstChild);
-        }
-        return parent;
+    while (parent.firstChild){
+        parent.removeChild(parent.firstChild);
     }
+    return parent;
+}
     
 
 function switchPosition(clickedReg){
-        //get the incumbent's data id
+    //get the incumbent's data id
+    let currentPlot = document.getElementById("current-plot");
+    let previousPlot = document.getElementById("previous-plot");
+    let formerPlot = document.getElementById("former-plot");
 
-        let currentPlot = document.getElementById("current-plot");
-        let previousPlot = document.getElementById("previous-plot");
+    // get region IDs for the plots stored in the id variable
+    // if they have id property, get that id number otherwise set it to -1
+    let currentRegId = currentPlot.dataset.hasOwnProperty("id") ? currentPlot.dataset.id : -1;
+    let previousRegId = previousPlot.dataset.hasOwnProperty("id") ? previousPlot.dataset.id : -1;
+    let formerRegId = formerPlot.dataset.hasOwnProperty("id") ? formerPlot.dataset.id : -1;
 
-        // get region IDs for the plots stored in the id variable
-        let currentRegId = currentPlot.dataset.hasOwnProperty("id") ? currentPlot.dataset.id : -1;
-        let previousRegId = previousPlot.dataset.hasOwnProperty("id") ? previousPlot.dataset.id : -1;
-
-        if (previousRegId != -1) {
-            JS9.ChangeRegions(previousRegId, { color: "black" });
-        }
-
-        currentPlot = removeAllChildNodes(currentPlot);
-        previousPlot = removeAllChildNodes(previousPlot);
-        
-        //load a plot to previous plot's region if there's a current plot
-        if (currentRegId != -1){
-            loadPlot(currentRegId, "previous-plot", "white");
-        }
-        loadPlot(clickedReg, "current-plot", "#A907F8");
-
+    if (formerRegId != -1) {
+        JS9.ChangeRegions(formerRegId, { color: "black" });
     }
+
+    currentPlot = removeAllChildNodes(currentPlot);
+    previousPlot = removeAllChildNodes(previousPlot);
+    formerPlot = removeAllChildNodes(formerPlot);
+    
+    //load the old plot to previous-plot's div if there's a current plot
+    if (currentRegId != -1){
+        loadPlot(currentRegId, "previous-plot", "#DC7C48");
+    }
+
+    if (previousRegId != -1) {
+        loadPlot(previousRegId, "former-plot", "#1DA5E2");
+    }
+    
+    loadPlot(clickedReg, "current-plot", "#A907F8");
+}
 
 
 function loadPlot(regionId, containerId, colour){
+    let plotPath = "cygnus/plots/reg";
+    let plotContainer = document.getElementById(containerId);
+    let plotTitle = document.createElement("div");
 
-        let plotPath = "cygnus/plots/reg";
-        let plotContainer = document.getElementById(containerId);
-        let plotTitle = document.createElement("div");
+    JS9.ChangeRegions(regionId, {color: colour});
 
-        JS9.ChangeRegions(regionId, {color: colour});
+    plotTitle.innerHTML = `Region ${regionId} <span style="display:inline-block; width:20px; height:20px; border-radius:50%; background-color:${colour}; vertical-align: middle;"></span>`;
+    plotTitle.style = "font-family: monospace; font-size: 13px;";
+    
+    let iframe = document.createElement("iframe");
+    Object.assign(iframe, {
+        // sandbox: "allow-same-origin allow-scripts",
+        width: "97vw",
+        height: "190%", scrolling: "no",
+        seamless: "seamless", frameborder: "0"
+    });
+    iframe.src = `http://${location.host}/${plotPath}${regionId}.html`;
 
-        plotTitle.innerHTML = `Region ${regionId} <span style="display:inline-block; width:20px; height:20px; border-radius:50%; background-color:${colour}; vertical-align: middle;"></span>`;
-        plotTitle.style = "font-family: monospace; font-size: 13px;";
-        
-        let iframe = document.createElement("iframe");
-        Object.assign(iframe, {
-            // sandbox: "allow-same-origin allow-scripts",
-            width: "97vw",
-            height: "190%", scrolling: "no",
-            seamless: "seamless", frameborder: "0"
-        });
-        iframe.src = `http://${location.host}/${plotPath}${regionId}.html`;
-
-        plotContainer = removeAllChildNodes(plotContainer);
-        plotContainer.dataset.id = regionId;
-        plotContainer.title = `Plot for region ${regionId}`;
-        plotContainer.appendChild(plotTitle);
-        plotContainer.appendChild(iframe);
-    }
+    plotContainer = removeAllChildNodes(plotContainer);
+    // add an id variable to the container
+    plotContainer.dataset.id = regionId;
+    plotContainer.title = `Plot for region ${regionId}`;
+    plotContainer.appendChild(plotTitle);
+    plotContainer.appendChild(iframe);
+}
 
 
 function lodLosPlots(im, xreg){
@@ -76,7 +83,6 @@ function lodLosPlots(im, xreg){
     else{
         alert("No plots available for the selected region");
     }
-
 }
 
 
