1c20e00d151896ed7566354aff64fee95cff1c65
mulan-94 <andatilexy@gmail.com>
2021-03-05 16:39:57 +0200

    Remember prefs files

---
+++ js9install/js9prefs.js
@@ -0,0 +1,35 @@
+var JS9Prefs = {
+	"globalOpts": {
+		"helperType": "nodejs",
+		"helperPort": 80,
+		"helperCGI": "./cgi-bin/js9/js9Helper.cgi",
+		"fits2png": false,
+		"debug": 0,
+		"loadProxy": true,
+		"workDir": "./data/tmp",
+		"workDirQuota": 100,
+		"fits2fits": "size>50",
+		"image": { "xdim": 4096, "ydim": 4096, "bin": 4},
+		"table": { "xdim": 4096, "ydim": 4096, "bin": 4},
+		"dataPath": "./data",
+		"analysisPlugins": "./analysis-plugins",
+		"analysisWrappers": "./analysis-wrappers",
+		"mousetouchZoom": true,
+		"mouseActions": ["display value/position", "pan the image", "change contrast/bias"],
+		"touchActions": ["display value/position", "pan the image", "change contrast/bias"],
+		"resetEmptyShapeId": true,
+		"requireFits2Fits": false
+	},
+
+	"imageOpts": {
+		"colormap": "inferno",
+		"scale": "linear",
+		"crosshair": true,
+		"zoom": "tofit"
+	},
+
+	"regionOpts": {
+		"panzoom": true,
+		"onmouseup": lodLosPlots
+	}
+}
