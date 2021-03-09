a5f269955f9c2e4bd9dd49d74ab6b76cfc911fa1
mulan-94 <andatilexy@gmail.com>
2021-03-09 14:24:03 +0200

    Lowers plots without re-rendering Match border colour to indicator color Add id to indicator color span

--- static/javascript/on_load.js
+++ static/javascript/on_load.js
@@ -1,61 +1,28 @@
-/*
-Load the cygnus image and region file after JS9 finishes loading
-*/
-
-
-function removeAllChildNodes(parent){
-    while (parent.firstChild){
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
     return parent;
 }
-    
-
-function switchPosition(clickedReg){
-    //clickedReg: numeric tag number of the clicked
-    //get the incumbent's data id
-    let currentPlot = document.getElementById("current-plot");
-    let previousPlot = document.getElementById("previous-plot");
-    let formerPlot = document.getElementById("former-plot");
-
-    // get region IDs (tags)for the plots stored in the id variable
-    // if they have id property, get that id number otherwise set it to -1
-    let currentRegId = currentPlot.dataset.hasOwnProperty("id") ? currentPlot.dataset.id : -1;
-    let previousRegId = previousPlot.dataset.hasOwnProperty("id") ? previousPlot.dataset.id : -1;
-    let formerRegId = formerPlot.dataset.hasOwnProperty("id") ? formerPlot.dataset.id : -1;
-
-    currentPlot = removeAllChildNodes(currentPlot);
-    previousPlot = removeAllChildNodes(previousPlot);
-    formerPlot = removeAllChildNodes(formerPlot);
-
-    if (formerRegId != -1) {
-        JS9.ChangeRegions(id=formerRegId, { color: "black" });
-    }
-    
-    //load the old plot to previous-plot's div if there's a current plot
-    if (previousRegId != -1) {
-        loadPlot(previousRegId, "former-plot", "#1DA5E2");
-    }
-    if (currentRegId != -1){
-        loadPlot(currentRegId, "previous-plot", "#DC7C48");
-    }
-    
-    loadPlot(clickedReg, "current-plot", "#A907F8");
-}
 
 
-function loadPlot(regionTag, containerId, colour){
+function createPlotContainer(regionTag, containerId, colour){
     // regionTag: the region's TAG number. Identical to its id number
-    let plotPath = "cygnus/plots/reg";
-    let plotContainer = document.getElementById(containerId);
-    let allContainer = document.getElementsByClassName(containerId.split("-")[0])[0];
+    let container = document.createElement("div");
+    
+    // container's children
     let plotTitle = document.createElement("div");
+    let plotHolder = document.createElement("div");
+    
+    // plotHOlder's child
+    let iframe = document.createElement("iframe");
+    let plotPath = "cygnus/plots/reg";
     let plot = `http://${location.host}/${plotPath}${regionTag}.html`;
+    
+    // console.log(`changing region ${regionTag} to colour ${colour}. Loading into ${containerId}`);
+    JS9.ChangeRegions(id = regionTag, { color: colour });
 
-    console.log(`changing region ${regionTag} to colour ${colour}. Loading into ${containerId}`);
-    JS9.ChangeRegions(id=regionTag, {color: colour});
-
-    plotTitle.innerHTML = `Region ${regionTag} <span style="display:inline-block;
+    plotTitle.innerHTML = `Region ${regionTag} <span id="colour-rep" style="display:inline-block;
                            width:15px; height:15px; border-radius:50%; 
                            background-color:${colour};">
                            </span>
@@ -66,61 +33,90 @@ function loadPlot(regionTag, containerId, colour){
                        position: absolute; height: 30%; text-align: end;
                        z-index: 1;`;
 
-    if (allContainer.childElementCount <2){
-        allContainer.prepend(plotTitle);
-    }
-    else{
-        allContainer.replaceChild(plotTitle, allContainer.childNodes[0]);
-    }
-    
-    let iframe = document.createElement("iframe");
     Object.assign(iframe, {
-        // sandbox: "allow-same-origin allow-scripts",
         width: "97vw",
         height: "200%", scrolling: "no",
-        seamless: "seamless", frameborder: "0"
+        seamless: "seamless", frameborder: "0",
     });
     iframe.src = plot;
 
-    plotContainer = removeAllChildNodes(plotContainer);
-    // add an id variable to the container
-    plotContainer.dataset.id = regionTag;
+    Object.assign(plotHolder, { "className": "plot-holder", "id": `${containerId}-plot` });
+    plotHolder.dataset.id = regionTag;
+    container.className = `plots ${containerId}`;
     
-    plotContainer.appendChild(iframe);
+    plotHolder.appendChild(iframe);
+    container.appendChild(plotTitle);
+    container.appendChild(plotHolder);
+    return container;
+
+    // // add an id variable to the container
+    // return [plotTitle, iframe];
+}
+
+function changeNames(parentElement, newName){
+    // change class and ids for the plot holder
+    // using this for the plots classes
+    parentElement.className = `plots ${newName}`;
+    parentElement.querySelector(".plot-holder").id = `${newName}-plot`;
+}
+
+function switchPosition(clickedRegionTag, containerId, colour){
+    let mainParent = document.querySelector(".plots-container");
+    // let names = {"current": "A907F8", "previous": "DC7C48", "former": "1DA5E2"};
+    let names = ["current", "previous", "former"];
+    let colours = ["#A907F8", "#DC7C48", "#1DA5E2"];
+
+
+    let plotContainer = createPlotContainer(clickedRegionTag, containerId, colour);
+    mainParent.removeChild(mainParent.lastElementChild);
+    mainParent.prepend(plotContainer);
+
+
+    for (let i=0; i<mainParent.childElementCount; i++){
+        if (mainParent.hasChildNodes()){
+            if (mainParent.children[i].querySelector("#colour-rep")){
+                mainParent.children[i].querySelector("#colour-rep").style.backgroundColor = colours[i];
+                mainParent.children[i].style.borderColor = colours[i];
+            }
+            changeNames(mainParent.children[i], names[i]);
+        }
+    }
     
 }
 
-
-function lodLosPlots(im, xreg){
+function lodLosPlots(im, xreg) {
     let xregTagNum = Number(xreg.tags[0]);
-    
+
+    // check if region is a pre determined line of site or an additional region
     if (xreg.tags[1] == "los") {
 
-        if (Number.isInteger(xregTagNum)){
+        if (Number.isInteger(xregTagNum)) {
             console.log(`Region ID: ${xreg.id} ${xregTagNum == xreg.id ? "does" : "doesn't"} match tag number: ${xregTagNum}`);
-            JS9.ChangeRegions(id=xregTagNum, { color: "#A907F8", strokeWidth: 5});
-            
-            switchPosition(xregTagNum);
+            // change the clicked region to this color and width
+            JS9.ChangeRegions(id = xregTagNum, { color: "#A907F8", strokeWidth: 5 });
+            switchPosition(xregTagNum, "current", "#A907F8");
         }
-        else{
+        else {
             alert("No plots available for the selected region");
         }
     }
 }
 
 
-function loadLosRegs(){
-    JS9.LoadRegions("./js9install/data/cyg_los-fk5.reg", {"selectable": false, 
-                                                "removeable": false, 
-                                                "strokeWidth":1});
+function loadLosRegs() {
+    JS9.LoadRegions("./js9install/data/cyg_los-fk5.reg", {
+        "selectable": false,
+        "removeable": false,
+        "strokeWidth": 1
+    });
 }
 
 
-function initialiseCygnus(){
+function initialiseCygnus() {
     JS9.ADDZOOM = 0.5;
     JS9.Preload("./js9install/data/nh-CYG-0.75-SLO-I.FITS",
         {
-            "zoom": "tofit", 
+            "zoom": "tofit",
             "colormap": "inferno",
             "scale": "linear", "scalemin": -0.009,
             "scalemax": 10, "onload": loadLosRegs,
